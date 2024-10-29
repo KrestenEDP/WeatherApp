023f644e69c6cc8a8ab8881ea170729013fff114
KrestenEDP <144691586+KrestenEDP@users.noreply.github.com>
2024-10-29 14:50:51 +0100

    alert screen

---
+++ app/src/main/java/dk/dtu/weatherapp/ui/alerts/alert.kt
@@ -0,0 +1,48 @@
+package dk.dtu.weatherapp.ui.alerts
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Alert(title: String, text: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier.padding(bottom = 4.dp)
+        ) {
+            Icon(
+                Icons.Default.ThumbUp, // @TODO use dynamic icon
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(end = 8.dp)
+                    .size(24.dp)
+            )
+            Text(
+                text = title,
+                fontSize = 24.sp
+            )
+        }
+        Text(text = text)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AlertPrev() {
+    Alert(
+        "Thunderstorm",
+        "Thunderstorm in Dalby 19:00 local time"
+    )
+}
--- app/src/main/java/dk/dtu/weatherapp/ui/alerts/alertsScreen.kt
+++ app/src/main/java/dk/dtu/weatherapp/ui/alerts/alertsScreen.kt
@@ -1,2 +1,50 @@
 package dk.dtu.weatherapp.ui.alerts
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+data class Alertdata(
+    val title: String,
+    val text: String
+)
+
+val alerts = arrayOf(
+    Alertdata("asd", "dsa"),
+    Alertdata("qwe", "ewq")
+)
+
+@Composable
+fun AlertsScreen(
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(alerts.size) { index ->
+            Alert(
+                title = alerts[index].title,
+                text = alerts[index].text
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AlertsScreenPrev() {
+    AlertsScreen(modifier = Modifier.fillMaxSize())
+}
