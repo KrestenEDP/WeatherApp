2af8cfe27733ebd73d5cf952050c92d4b6504b3f
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2024-11-17 13:27:53 +0100

    Added custom animations for navigation

--- app/src/main/java/dk/dtu/weatherapp/navigation/WeatherNavHost.kt
+++ app/src/main/java/dk/dtu/weatherapp/navigation/WeatherNavHost.kt
@@ -1,13 +1,17 @@
 package dk.dtu.weatherapp.navigation
 
 import android.util.Log
+import androidx.annotation.AnimRes
+import androidx.annotation.AnimatorRes
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import dk.dtu.weatherapp.R
 import dk.dtu.weatherapp.ui.alerts.AlertsScreen
 import dk.dtu.weatherapp.ui.forecast.SingleDayForecastScreen
 import dk.dtu.weatherapp.ui.homepage.Homepage
@@ -59,14 +63,21 @@ fun WeatherNavHost(
     }
 }
 
-fun NavHostController.navigateSingleTopTo(route: String) =
+fun NavHostController.navigateSingleTopTo(route: String) {
     this.navigate(route) {
         /*popUpTo(this@navigateSingleTopTo.graph.findStartDestination().id) {
             saveState = true
         }*/
         launchSingleTop = true
         //restoreState = true
+        anim {
+            enter = R.anim.nav_default_enter_anim
+            exit = R.anim.nav_default_exit_anim
+            popEnter = R.anim.nav_default_pop_enter_anim
+            popExit = R.anim.nav_default_pop_exit_anim
+        }
     }
+}
 
 private fun NavHostController.navigateToSingleDayForecast(index: Int) {
     this.navigateSingleTopTo("${SingleDayForecast.route}/$index")
