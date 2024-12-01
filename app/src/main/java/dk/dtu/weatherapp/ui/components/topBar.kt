6da3e09a07f3326e1f09d40bfc3d243953bc8cab
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2024-12-01 12:28:14 +0100

    Changes to topbar appearance

--- app/src/main/java/dk/dtu/weatherapp/ui/components/topBar.kt
+++ app/src/main/java/dk/dtu/weatherapp/ui/components/topBar.kt
@@ -1,5 +1,7 @@
 package dk.dtu.weatherapp.ui.components
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -7,10 +9,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import dk.dtu.weatherapp.navigation.Weather
@@ -27,20 +29,25 @@ fun MyTopAppBar(
         title = {
             Text(
                 text = currentDestination.appBarTitle,
-                style = Typography.titleLarge,
-                color = Color.White
+                style = Typography.displaySmall,
             )
         },
         navigationIcon = {
             if (currentDestination.showBackButton) {
-                IconButton(onClick = { navController.navigateUp() }) {
-                    Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back", tint = Color.White)
+                IconButton(
+                    onClick = { navController.navigateUp() },
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Icon(
+                        Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Back",
+                        modifier = Modifier
+                            .size(36.dp)
+                            .padding(start = 4.dp)
+                    )
                 }
             }
         },
-        colors = topAppBarColors(
-            containerColor = Color(0xFF6200EE),
-            ),
     )
 }
 
