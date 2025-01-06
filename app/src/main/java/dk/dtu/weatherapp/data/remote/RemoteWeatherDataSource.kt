27e5217e156ab43c27ddf6c38afdbe609c898079
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2025-01-06 11:48:34 +0100

    Weather data source

---
+++ app/src/main/java/dk/dtu/weatherapp/data/remote/RemoteWeatherDataSource.kt
@@ -0,0 +1,28 @@
+package dk.dtu.weatherapp.data.remote
+
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+
+class RemoteWeatherDataSource {
+    companion object {
+        const val BASE_URL = "https://api.openweathermap.org"
+        private const val CONTENT_TYPE = "application/json; charset=UTF8"
+    }
+
+    private val json = Json {
+        ignoreUnknownKeys = true
+    }
+
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(
+            json.asConverterFactory(CONTENT_TYPE.toMediaType())
+        )
+        .baseUrl(BASE_URL)
+        .build()
+
+    private val weatherApi: WeatherApiService = retrofit.create(WeatherApiService::class.java)
+
+    suspend fun getWeather() = weatherApi.getWeather()
+}
