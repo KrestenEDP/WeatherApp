73e5088c2353b352651cc2bacccd736ebda494a0
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2025-01-07 16:28:42 +0100

    Simple caching for offline use

--- app/src/main/java/dk/dtu/weatherapp/data/remote/RemoteWeatherDataSource.kt
+++ app/src/main/java/dk/dtu/weatherapp/data/remote/RemoteWeatherDataSource.kt
@@ -1,7 +1,13 @@
 package dk.dtu.weatherapp.data.remote
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import dk.dtu.weatherapp.getAppContext
 import kotlinx.serialization.json.Json
+import okhttp3.Cache
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
 
@@ -15,20 +21,38 @@ class RemoteWeatherDataSource {
         ignoreUnknownKeys = true
     }
 
+    val context: Context = getAppContext()
+
     private val cacheSize = (10 * 1024 * 1024).toLong() // 10 MB
-    //private val cache = Cache(context.cacheDir, cacheSize)
-    //private val okHttpClient = OkHttpClient.Builder()
-        //.cache(cache)
-        //.build()
+    private val cache = Cache(context.cacheDir, cacheSize)
+    private val okHttpClient = OkHttpClient.Builder()
+        .cache(cache)
+        .addInterceptor { chain ->
+            var request = chain.request()
+            request = if (hasNetwork(context)!!)
+                request.newBuilder().header("Cache-Control", "public, max-age=" + 300).build()
+            else
+                request.newBuilder().header("Cache-Control", "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7).build()
+            chain.proceed(request)
+        }
+        .build()
 
     private val retrofit = Retrofit.Builder()
         .addConverterFactory(
             json.asConverterFactory(CONTENT_TYPE.toMediaType())
-
         )
-        //.client(okHttpClient)
+        .client(okHttpClient)
         .baseUrl(BASE_URL)
         .build()
+
+    fun hasNetwork(context: Context): Boolean? {
+        var isConnected: Boolean? = false // Initial Value
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+        if (activeNetwork != null && activeNetwork.isConnected)
+            isConnected = true
+        return isConnected
+    }
 
     private val weatherApi: WeatherApiService = retrofit.create(WeatherApiService::class.java)
 
