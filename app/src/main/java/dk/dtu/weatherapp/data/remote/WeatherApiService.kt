aa0e8c3aba699e7d544255fdca79f4ff30b73f38
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2025-01-08 11:33:38 +0100

    Unit type is a query parameter now

--- app/src/main/java/dk/dtu/weatherapp/data/remote/WeatherApiService.kt
+++ app/src/main/java/dk/dtu/weatherapp/data/remote/WeatherApiService.kt
@@ -6,22 +6,25 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherApiService {
-    @GET("/data/2.5/weather?units=metric&appid=$ApiKey")
+    @GET("/data/2.5/weather?appid=$ApiKey")
     suspend fun getCurrentWeather(
         @Query("lat") lat: Double = 55.77,
-        @Query("lon") lon: Double = 12.50
+        @Query("lon") lon: Double = 12.50,
+        @Query("units") units: String = "metric"
     ): WeatherHourDao
 
-    @GET("/data/2.5/forecast/daily?cont=16&units=metric&appid=$ApiKey")
+    @GET("/data/2.5/forecast/daily?cont=16&appid=$ApiKey")
     suspend fun getDailyWeather(
         @Query("lat") lat: Double = 55.77,
-        @Query("lon") lon: Double = 12.50
+        @Query("lon") lon: Double = 12.50,
+        @Query("units") units: String = "metric"
         ): DailyWeatherDao
 
-    @GET("/data/2.5/forecast/hourly?units=metric&appid=$ApiKey")
+    @GET("/data/2.5/forecast/hourly?appid=$ApiKey")
     suspend fun getHourlyWeather(
         @Query("lat") lat: Double = 55.77,
         @Query("lon") lon: Double = 12.50,
-        @Query("cnt") count: Int = 96
+        @Query("cnt") count: Int = 96,
+        @Query("units") units: String = "metric"
     ): HourlyWeatherDao
 }
