3f128bcb003a2aa7cd0debb32bf1f7daa88806bc
KrestenEDP <144691586+KrestenEDP@users.noreply.github.com>
2025-01-09 14:13:32 +0100

    New page stats instead of map

--- app/src/main/java/dk/dtu/weatherapp/domain/StatsRepository.kt
+++ app/src/main/java/dk/dtu/weatherapp/domain/StatsRepository.kt
@@ -3,50 +3,24 @@ package dk.dtu.weatherapp.domain
 import dk.dtu.weatherapp.data.model.StatsListDao
 import dk.dtu.weatherapp.data.remote.RemoteStatisticsDataSource
 import dk.dtu.weatherapp.models.StatsDay
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
 
 class StatsRepository {
     private val dataSource = RemoteStatisticsDataSource()
 
-    private val mutableStatsFlow = MutableSharedFlow<List<StatsDay>>()
-    val currentStatsFlow = mutableStatsFlow.asSharedFlow()
+    suspend fun getDayStats(lat: Double, lon: Double, month: Int, day: Int): List<StatsDay> {
+         return dataSource.getDayStatistics(lat = lat, lon = lon, month = month, day = day)
+             .mapToStatsDay()
+    }
 
-    suspend fun getDayStats(
-        lat: Double,
-        lon: Double,
-        month: Int,
-        day: Int
-        ) = mutableStatsFlow.emit(
-        dataSource.getDayStatistics(
-            lat = lat,
-            lon = lon,
-            month = month,
-            day = day
-        ).mapToStatsDay()
-    )
+    suspend fun getMonthStats(lat: Double, lon: Double, month: Int): List<StatsDay> {
+        return dataSource.getMonthStatistics(lat = lat, lon = lon, month = month)
+            .mapToStatsDay()
+    }
 
-    suspend fun getMonthStats(
-        lat: Double,
-        lon: Double,
-        month: Int
-        ) = mutableStatsFlow.emit(
-        dataSource.getMonthStatistics(
-            lat = lat,
-            lon = lon,
-            month = month
-        ).mapToStatsDay()
-    )
-
-    suspend fun getYearStats(
-        lat: Double,
-        lon: Double
-        ) = mutableStatsFlow.emit(
-        dataSource.getYearStatistics(
-            lat = lat,
-            lon = lon
-        ).mapToStatsDay()
-    )
+    suspend fun getYearStats(lat: Double, lon: Double): List<StatsDay> {
+        return dataSource.getYearStatistics(lat = lat, lon = lon)
+            .mapToStatsDay()
+    }
 }
 
 fun StatsListDao.mapToStatsDay(): List<StatsDay> {
