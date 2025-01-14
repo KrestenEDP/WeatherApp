4c1ecf42172120bc8b5b34a972fb7f24f0ea86e0
KrestenEDP <144691586+KrestenEDP@users.noreply.github.com>
2025-01-14 12:25:28 +0100

    working non cycling list

---
+++ app/src/main/java/dk/dtu/weatherapp/ui/components/CircularList.kt
@@ -0,0 +1,95 @@
+package dk.dtu.weatherapp.ui.components
+
+import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+/**
+ * A circular list that can be scrolled infinitely in both directions.
+ * @param items The items to display in the list.
+ * @param textSize The size of the text in the list.
+ * @param itemHeight The height of each item in the wheel.
+ * @param itemDisplayCount The number of items to display at once. Should be an odd number.
+ */
+@Composable
+fun CircularList(
+    items: MutableList<String>,
+    textSize: Int = 24,
+    itemHeight: Int = 40,
+    itemDisplayCount: Int = 3,
+    width: Int = 140,
+    modifier: Modifier = Modifier,
+    onItemSelected: (String) -> Unit = {}
+) {
+    val scrollState = rememberLazyListState(0)
+    var lastSelectedIndex by remember { mutableIntStateOf(0) }
+    items.add(0, "")
+    items.add("")
+    LaunchedEffect(scrollState) {
+        snapshotFlow { scrollState.firstVisibleItemIndex }
+            .collect { firstVisibleItemIndex ->
+                lastSelectedIndex = (firstVisibleItemIndex + itemDisplayCount / 2)
+                onItemSelected(items[lastSelectedIndex])
+            }
+    }
+
+    LazyColumn(
+        state = scrollState,
+        flingBehavior = rememberSnapFlingBehavior(lazyListState = scrollState),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .height((itemHeight * itemDisplayCount).dp)
+            .width(width.dp)
+    ) {
+        items(items.size) { index ->
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = modifier
+                    .height(itemHeight.dp)
+            ) {
+                Text(
+                    text = items[index],
+                    fontSize = textSize.sp,
+                    color =  if(lastSelectedIndex == index) {
+                        MaterialTheme.colorScheme.onBackground
+                    } else {
+                        Color.Gray
+
+                    },
+                    textAlign = TextAlign.Center,
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CircularListPreview() {
+    CircularList(
+        items = listOf(
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December").toMutableList(),
+        itemHeight = 60,
+        itemDisplayCount = 3,
+    )
+}
--- app/src/main/java/dk/dtu/weatherapp/ui/statistics/DayStats.kt
+++ app/src/main/java/dk/dtu/weatherapp/ui/statistics/DayStats.kt
@@ -24,15 +24,23 @@ import androidx.compose.material3.TooltipDefaults
 import androidx.compose.material3.TooltipState
 import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dk.dtu.weatherapp.GlobalUnits
 import dk.dtu.weatherapp.R
+import dk.dtu.weatherapp.ui.components.CircularList
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -44,22 +52,19 @@ fun DayStats() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
-            .then(if(tooltipState.isVisible) Modifier.blur(2.dp) else Modifier)
+            .then(if (tooltipState.isVisible) Modifier.blur(2.dp) else Modifier)
     ){
         TooltipBox(
             positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
             tooltip = {
                 RichTooltip(
-                    title = { Text("What is this?") },
-                    text = { Text("Choose a date to see statistics for the weather on that" +
-                            " day. This does not reflect the actual weather on that day but a" +
-                            " calculated value of the average weather for that day.") },
+                    title = { Text(stringResource(R.string.statsTooltipTitle)) },
+                    text = { Text(stringResource(R.string.statsTooltipText)) },
                     action = {
                         TextButton(
                             onClick = { tooltipState.dismiss() }
                         ) {
-                            Text("Dismiss")
+                            Text(stringResource(R.string.statsTooltipButton))
                         }
                     },
                     modifier = Modifier
@@ -75,7 +80,7 @@ fun DayStats() {
                     .fillMaxWidth()
                     .padding(bottom = 16.dp)
             ){
-                Text("Someway to choose a day")
+                DayPicker()
                 IconButton(
                     onClick = { CoroutineScope(Dispatchers.Main).launch { tooltipState.show() } }
                 ) {
@@ -95,11 +100,67 @@ fun DayStats() {
         ) {
             item {StatsCard(title = "Temperature", value = 20.0, R.drawable.i01d, GlobalUnits.temp)} // TODO use imported values
             item {StatsCard(title = "Wind", value = 4.0, R.drawable.wind, GlobalUnits.wind)} // TODO use imported values
-            item {StatsCard(title = "Precipitation", value = 0.0, R.drawable.i09d, GlobalUnits.precipitation)} // TODO use imported values
+            item {StatsCard(title = "Rain", value = 0.0, R.drawable.i09d, GlobalUnits.precipitation)} // TODO use imported values
             item {StatsCard(title = "Humidity", value = 80.51, R.drawable.humidity, "%")} // TODO use imported values
             item {StatsCard(title = "Pressure", value = 1027.0, R.drawable.compress, "hPa")} // TODO use imported values
             item {StatsCard(title = "Clouds", value = 90.2, R.drawable.i03d, "%")} // TODO use imported values
         }
+    }
+}
+
+@Composable
+fun DayPicker() {
+    var listOfDays: MutableList<String> by remember { mutableStateOf(MutableList(1) { "" }) }
+    val listOfMonths = listOf(
+        "January", "February", "March", "April", "May", "June", "July", "August", "September",
+        "October", "November", "December"
+    ).toMutableList()
+    var day by remember { mutableIntStateOf(1)}
+    var month by remember { mutableIntStateOf(1)}
+    listOfDays = when (month) {
+        4, 6, 9, 11 -> listOf(
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+            "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+            "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"
+        ).toMutableList()
+        2 -> listOf(
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+            "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+            "21", "22", "23", "24", "25", "26", "27", "28"
+        ).toMutableList()
+        else -> listOf(
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+            "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+            "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"
+        ).toMutableList()
+    }
+    Row {
+        CircularList(
+            items = listOfDays,
+            itemHeight = 40,
+            itemDisplayCount = 3,
+            width = 60,
+        ) { day = it.toInt() }
+        CircularList(
+            items = listOfMonths,
+            itemHeight = 40,
+            itemDisplayCount = 3,
+            width = 140,
+        ) { month = when(it) {
+            "January" -> 1
+            "February" -> 2
+            "March" -> 3
+            "April" -> 4
+            "May" -> 5
+            "June" -> 6
+            "July" -> 7
+            "August" -> 8
+            "September" -> 9
+            "October" -> 10
+            "November" -> 11
+            "December" -> 12
+            else -> -1
+        } }
     }
 }
 
@@ -109,20 +170,29 @@ fun StatsCard(title: String, value: Double, icon: Int, unit: String) {
         elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
         modifier = Modifier.fillMaxWidth()
     ) {
-        Row(modifier = Modifier.padding(16.dp)) {
-            Icon(
-                painter = painterResource(icon),
-                contentDescription = "null",
-                modifier = Modifier
-                    .size(24.dp)
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Icon(
+                    painter = painterResource(icon),
+                    contentDescription = "null",
+                    modifier = Modifier
+                        .size(16.dp)
+                )
+                Text(
+                    title,
+                    fontSize = 12.sp,
+                    modifier = Modifier.padding(start = 4.dp)
+                )
+            }
+            Text(
+                text = "$value $unit",
+                fontWeight = FontWeight.Bold
             )
-            Text(title, modifier = Modifier.padding(start = 4.dp))
         }
-        Text(
-            text = "$value $unit",
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
-        )
     }
 }
 
