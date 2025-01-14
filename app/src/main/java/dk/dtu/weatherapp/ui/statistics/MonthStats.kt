4f46923094594372bf0eb9f94f2e2f5a57d13d27
KrestenEDP <144691586+KrestenEDP@users.noreply.github.com>
2025-01-14 15:39:24 +0100

    start of stats for month

--- app/src/main/java/dk/dtu/weatherapp/ui/statistics/MonthStats.kt
+++ app/src/main/java/dk/dtu/weatherapp/ui/statistics/MonthStats.kt
@@ -1,8 +1,45 @@
 package dk.dtu.weatherapp.ui.statistics
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import dk.dtu.weatherapp.ui.components.CircularList
 
 @Composable
 fun MonthStats() {
+    var month by remember { mutableIntStateOf(1) }
+    MonthPicker(onMonthChange = { month = it })
+}
 
+@Composable
+fun MonthPicker(onMonthChange: (Int) -> Unit) {
+    val months = listOf(
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ).toMutableList()
+    CircularList(
+        items = months,
+        itemDisplayCount = 3,
+        width = 140,
+        onItemSelected = { onMonthChange(
+            when (it) {
+                "January" -> 1
+                "February" -> 2
+                "March" -> 3
+                "April" -> 4
+                "May" -> 5
+                "June" -> 6
+                "July" -> 7
+                "August" -> 8
+                "September" -> 9
+                "October" -> 10
+                "November" -> 11
+                "December" -> 12
+                else -> 1
+            }
+        ) }
+    )
 }
