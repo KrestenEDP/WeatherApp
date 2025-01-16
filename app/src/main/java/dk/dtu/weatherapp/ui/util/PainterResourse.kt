e4dd9345efa22d9288db7e35292cf2f2f6ed2964
KrestenEDP <144691586+KrestenEDP@users.noreply.github.com>
2025-01-16 15:25:54 +0100

    new launcher icon

--- app/src/main/java/dk/dtu/weatherapp/ui/util/PainterResourse.kt
+++ app/src/main/java/dk/dtu/weatherapp/ui/util/PainterResourse.kt
@@ -1,13 +1,9 @@
 package dk.dtu.weatherapp.ui.util
 
 import android.content.Context
-import dk.dtu.weatherapp.R
 
 
 fun getPainterResource(id: String, context: Context): Int {
     val icon = context.resources.getIdentifier(id, "drawable", context.packageName)
-    if (icon == 0) {
-        return R.drawable.ic_launcher_foreground
-    }
     return icon
 }
