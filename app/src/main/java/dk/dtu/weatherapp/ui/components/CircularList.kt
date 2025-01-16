67f7ab987bd0c6ebe0c39ff6defd243719f1e6e9
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2025-01-16 14:00:33 +0100

    Added border to circularlist for fade effect

--- app/src/main/java/dk/dtu/weatherapp/ui/components/CircularList.kt
+++ app/src/main/java/dk/dtu/weatherapp/ui/components/CircularList.kt
@@ -1,5 +1,6 @@
 package dk.dtu.weatherapp.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
@@ -17,6 +18,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -52,33 +54,50 @@ fun CircularList(
             }
     }
 
-    LazyColumn(
-        state = scrollState,
-        flingBehavior = rememberSnapFlingBehavior(lazyListState = scrollState),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
-            .height((itemHeight * itemDisplayCount).dp)
-            .width(width.dp)
-    ) {
-        items(items.size) { index ->
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = modifier
-                    .height(itemHeight.dp)
-            ) {
-                Text(
-                    text = items[index],
-                    style = Typography.titleLarge,
-                    color =  if(lastSelectedIndex == index) {
-                        MaterialTheme.colorScheme.onBackground
-                    } else {
-                        Color.Gray
+    Box {
+        LazyColumn(
+            state = scrollState,
+            flingBehavior = rememberSnapFlingBehavior(lazyListState = scrollState),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier
+                .height((itemHeight * itemDisplayCount).dp)
+                .width(width.dp)
+        ) {
+            items(items.size) { index ->
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = modifier
+                        .height(itemHeight.dp)
+                ) {
+                    Text(
+                        text = items[index],
+                        style = Typography.titleLarge,
+                        color =  if(lastSelectedIndex == index) {
+                            MaterialTheme.colorScheme.onBackground
+                        } else {
+                            Color.Gray
 
-                    },
-                    textAlign = TextAlign.Center,
-                )
+                        },
+                        textAlign = TextAlign.Center,
+                    )
+                }
             }
         }
+        Box(
+            modifier = Modifier
+                .height((itemHeight * itemDisplayCount).dp)
+                .width(width.dp)
+                .background(
+                    brush = Brush.verticalGradient(
+                        colorStops = arrayOf(
+                            0.0f to MaterialTheme.colorScheme.background,
+                            0.2f to Color.Transparent,
+                            0.8f to Color.Transparent,
+                            1f to MaterialTheme.colorScheme.background
+                        )
+                    )
+                )
+        )
     }
 }
 
