2e2d1ac4f6ba03be863632959818a93b9368d44d
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2025-01-17 15:03:42 +0100

    Fix for alert icon

--- app/src/main/java/dk/dtu/weatherapp/ui/alerts/alert.kt
+++ app/src/main/java/dk/dtu/weatherapp/ui/alerts/alert.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dk.dtu.weatherapp.models.Alert
@@ -29,7 +28,6 @@ fun Alert(alert: Alert, modifier: Modifier = Modifier) {
             Icon(
                 Icons.Filled.Warning,
                 contentDescription = null,
-                tint = Color.Unspecified,
                 modifier = Modifier
                     .padding(end = 8.dp)
                     .size(36.dp)
